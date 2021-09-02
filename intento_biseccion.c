057d6b8729e03bff459ff45e5a678ee46f230c35
sergioaer <saelizondor@gmail.com>
2021-09-02 18:37:03 -0500

    Se cambió función de scan por lectura de string

--- intento_biseccion.c
+++ intento_biseccion.c
@@ -1,40 +1,41 @@
-//METODO BISECCION DE LA FORMA X^3+X^2+X+C
+//MÉTODO BISECCIÓN DE LA FORMA X^3+X^2+X+C
 #include <stdio.h> 
 #include <stdlib.h>
-#include <math.h>  
+#include <math.h>
 
 double iteracion_anterior=1,iteracion_actual=1;
 int iteracion=0;
 
+double leer_doble();
 int validacion_intervalo(double x1,double xu,double c1,double c2,double c3,double c4);
 double calcular_xr(double x1,double xu);
 double casos(double x1,double xr,double c1,double c2,double c3,double c4);
 double error_aprox(double iteracion_actual,double iteracion_anterior);
 
 int main(){
-	
 	double error_aproximado=100,validacion=0,res=0,raiz=0;
 	double x1=0,xu=0,xr=0,Ea=0,c1=0,c2=0,c3=0,c4=0;
+
 	
 	printf("\t--------------METODO DE BISECCION-------------\n\n");
-	printf("\t\tDA SALUCION A LA FORMA x%c+x%c+x+c\n\n",252,253);
+	printf("\t\tDA SOLUCION A LA FORMA x%c+x%c+x+c\n\n",252,253);
 	
 	do{
-		printf("\tIngresa el valor minimo de tu intevalo: ");
-		scanf("%lf",&x1);
+		printf("\tIngresa el valor minimo de tu intervalo: ");
+        x1 = leer_doble();
 		printf("\n\tIngresa el valor maximo de tu intervalo: ");
-		scanf("%lf",&xu);
+		xu = leer_doble();
 		printf("\n\tIngresa el maximo valor del error aproximado que puede tener: ");
-		scanf("%lf",&Ea);
+		Ea = leer_doble();
 	
 		printf("\n\n\tIngresa el coeficiente del termino x%c: ",252);
-		scanf("%lf",&c1);
+		c1 = leer_doble();
 		printf("\n\tIngresa el coeficiente del termino x%c: ",253);
-		scanf("%lf",&c2);
+		c2 = leer_doble();
 		printf("\n\tIngresa el coeficiente del termino x: ");
-		scanf("%lf",&c3);
+		c3 = leer_doble();
 		printf("\n\tIngresa el valor de tu constante: ");
-		scanf("%lf",&c4);
+		c4 = leer_doble();
 		validacion=validacion_intervalo(x1,xu,c1,c2,c3,c4);
 	}while(validacion==0);
 	
@@ -65,6 +66,13 @@ int main(){
 	printf("\n\tError aproximado: %lf%c",error_aproximado,37);
 	
 	return 0;
+}
+
+double leer_doble() {
+    char cadena[127];   // cadena temporal para leer valores
+    char *ap; // apuntador necesario para convertir a real
+    fgets(cadena, 127, stdin);
+    return strtod(cadena, &ap);
 }
 
 int validacion_intervalo(double x1,double xu,double c1,double c2,double c3,double c4){
